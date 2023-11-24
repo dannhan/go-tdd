c022354c5c5dbc5ee0bfe019d36cdb2a8da95315
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2023-11-24 23:10:09 +0700

    refactor hello func to handler empty string

--- hello.go
+++ hello.go
@@ -5,6 +5,10 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		return englishHelloPrefix + "world"
+	}
+
 	return englishHelloPrefix + name
 }
 
